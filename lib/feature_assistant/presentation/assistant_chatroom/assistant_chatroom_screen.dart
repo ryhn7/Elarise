6416c5d8f1e90b42b7533e6d303fd6c2563f102c
Rayhan Illyas <rayhan205732@gmail.com>
2024-01-09 13:27:10 +0700

    feat(chatroom): wip assistant chatroom

---
+++ lib/feature_assistant/presentation/assistant_chatroom/assistant_chatroom_screen.dart
@@ -0,0 +1,144 @@
+import 'package:elarise/theme/colors.dart';
+import 'package:elarise/theme/style.dart';
+import 'package:flutter/material.dart';
+
+class AssistantChatroomScreen extends StatelessWidget {
+  const AssistantChatroomScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget appBar() {
+      return AppBar(
+        backgroundColor: darkGray,
+        shape: Border(bottom: BorderSide(color: primary, width: 1)),
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_rounded, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Row(
+          children: [
+            Image.asset("assets/images/dummy_logo_header.png",
+                width: 40, height: 40),
+            const SizedBox(width: 16),
+            Text(
+              "Elara AI",
+              style: getSansFranciscoSemiBold20(color: Colors.white),
+            )
+          ],
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.more_vert,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          )
+        ],
+      );
+    }
+
+    Widget responseMessage() {
+      return Container(
+          margin: const EdgeInsets.only(top: 24, left: 16),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.asset("assets/images/dummy_logo.png",
+                      width: 24, height: 24),
+                  const SizedBox(width: 12),
+                  Text(
+                    "Elara AI",
+                    style: getSansFranciscoSemiBold16(color: Colors.white),
+                  )
+                ],
+              ),
+              const SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Container(
+                      constraints: BoxConstraints(
+                        maxWidth: MediaQuery.of(context).size.width * 0.7,
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 12),
+                      decoration: BoxDecoration(
+                        color: blackOlive,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(30)),
+                      ),
+                      child: Text("Hello, I'm Elara your virtual assistant.",
+                          style:
+                              getSansFranciscoRegular16(color: Colors.white)),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ));
+    }
+
+    Widget requestMessage() {
+      return Container(
+        margin: const EdgeInsets.only(top: 48, right: 16),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Image.asset("assets/images/dummy_avatar.png",
+                    width: 24, height: 24),
+                const SizedBox(width: 12),
+                Text(
+                  "You",
+                  style: getSansFranciscoSemiBold16(color: Colors.white),
+                )
+              ],
+            ),
+            const SizedBox(height: 12),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    constraints: BoxConstraints(
+                      maxWidth: MediaQuery.of(context).size.width * 0.7,
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 12),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                    child: Text("Hi Elara, I need help with my order.",
+                        style: getSansFranciscoRegular16(color: Colors.black)),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: darkGray,
+      body: SingleChildScrollView(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          appBar(),
+          responseMessage(),
+          requestMessage(),
+        ]),
+      ),
+    );
+  }
+}
--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -7,3 +7,5 @@ Color neutralOne = const Color(0xFFF3EEE9);
 Color neutralTwo = const Color(0x47776F69);
 Color neutralThree = const Color(0xFF776F69);
 Color neutralFour = const Color(0xFF776F69);
+Color darkGray = const Color(0xFF1e1e1e);
+Color blackOlive = const Color(0xFF3c3c3c);
