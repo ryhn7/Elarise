fe9698bedff021dda0660bbc96ca7dabe1f751ec
Rayhan Illyas <rayhan205732@gmail.com>
2024-02-06 23:22:48 +0700

    feat(setting): DI use case logout

---
+++ lib/di/usecases/setting_usecases/logout_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'logout_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$logoutHash() => r'446d08287b7b251f2d7938e2d908f310c942f68d';
+
+/// See also [logout].
+@ProviderFor(logout)
+final logoutProvider = AutoDisposeProvider<Logout>.internal(
+  logout,
+  name: r'logoutProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$logoutHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef LogoutRef = AutoDisposeProviderRef<Logout>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
