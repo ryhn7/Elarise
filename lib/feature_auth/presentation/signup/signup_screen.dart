08f92866dd3a6592756c202dac6eda9715ec6624
Rayhan Illyas <rayhan205732@gmail.com>
2024-02-06 23:16:28 +0700

    refactor: link to login screen

--- lib/feature_auth/presentation/signup/signup_screen.dart
+++ lib/feature_auth/presentation/signup/signup_screen.dart
@@ -4,79 +4,84 @@ import 'package:elarise/feature_auth/presentation/widget/google_auth_button.dart
 import 'package:elarise/theme/colors.dart';
 import 'package:elarise/theme/style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SignUpScreen extends StatelessWidget {
+import '../../../router/router_provider.dart';
+
+class SignUpScreen extends ConsumerWidget {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController userNameController = TextEditingController();
 
   SignUpScreen({super.key});
 
-  Widget header() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Image.asset("assets/images/dummy_logo_header.png",
-            width: 64, height: 64),
-        const SizedBox(height: 8),
-        Text(
-          "Create your account",
-          style: getGrotesqueSemiBoldStyle40(color: neutralFour),
-        ),
-        Row(
-          children: [
-            Text(
-              "Already have an account?",
-              style: getSansFranciscoRegular16(color: darkSilver),
-            ),
-            TextButton(
-                onPressed: () {},
-                style: TextButton.styleFrom(
-                  padding: EdgeInsets.zero,
-                  foregroundColor: primary,
-                ),
-                child: Text(
-                  "Login",
-                  style: getSansFranciscoSemiBold16(color: primary),
-                )),
-          ],
-        )
-      ],
-    );
-  }
-
-  Widget dividerSignUpAlternate() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Flexible(
-          child: Divider(
-            color: neutralThree30,
-            height: 1,
-            thickness: 1,
-            indent: 16,
-          ),
-        ),
-        const SizedBox(width: 12),
-        Text(
-          "Or sign up with",
-          style: getSansFranciscoRegular14(color: neutralThree),
-        ),
-        const SizedBox(width: 12),
-        Flexible(
-          child: Divider(
-            color: neutralThree30,
-            height: 1,
-            thickness: 1,
-            endIndent: 16,
-          ),
-        ),
-      ],
-    );
-  }
-
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    Widget header() {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset("assets/images/dummy_logo_header.png",
+              width: 64, height: 64),
+          const SizedBox(height: 8),
+          Text(
+            "Create your account",
+            style: getGrotesqueSemiBoldStyle40(color: neutralFour),
+          ),
+          Row(
+            children: [
+              Text(
+                "Already have an account?",
+                style: getSansFranciscoRegular16(color: darkSilver),
+              ),
+              TextButton(
+                  onPressed: () {
+                    ref.read(routerProvider).goNamed('login');
+                  },
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    foregroundColor: primary,
+                  ),
+                  child: Text(
+                    "Login",
+                    style: getSansFranciscoSemiBold16(color: primary),
+                  )),
+            ],
+          )
+        ],
+      );
+    }
+
+    Widget dividerSignUpAlternate() {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Flexible(
+            child: Divider(
+              color: neutralThree30,
+              height: 1,
+              thickness: 1,
+              indent: 16,
+            ),
+          ),
+          const SizedBox(width: 12),
+          Text(
+            "Or sign up with",
+            style: getSansFranciscoRegular14(color: neutralThree),
+          ),
+          const SizedBox(width: 12),
+          Flexible(
+            child: Divider(
+              color: neutralThree30,
+              height: 1,
+              thickness: 1,
+              endIndent: 16,
+            ),
+          ),
+        ],
+      );
+    }
+
     return Scaffold(
       backgroundColor: neutralOneAlt,
       body: SafeArea(
