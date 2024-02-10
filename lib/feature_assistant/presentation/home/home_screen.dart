42c8de085030e453e4e45314df49fec41153f30a
Rayhan Illyas <rayhan205732@gmail.com>
2024-02-10 20:55:08 +0700

    refactor: adjust home screen

--- lib/feature_assistant/presentation/home/home_screen.dart
+++ lib/feature_assistant/presentation/home/home_screen.dart
@@ -1,5 +1,4 @@
-import 'package:elarise/feature_assistant/presentation/home/home_state.dart';
-import 'package:elarise/feature_assistant/presentation/home/home_state_notifier.dart';
+import 'package:elarise/di/repositories/user_datastore_repository/user_datastore_repository_provider.dart';
 import 'package:elarise/feature_assistant/presentation/home/widget/chatroom_card.dart';
 import 'package:elarise/router/router_provider.dart';
 import 'package:elarise/theme/colors.dart';
@@ -7,172 +6,176 @@ import 'package:elarise/theme/style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends ConsumerStatefulWidget {
+import '../../../core/domain/entities/user_preferences.dart';
+
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  ConsumerState<HomeScreen> createState() => _HomeScreenState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
 
-class _HomeScreenState extends ConsumerState<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    ref.listen<HomeState>(homeStateNotifierProvider, (previous, next) {
-      if (next.user == null && next.error == null) {
-        ref.read(routerProvider).goNamed('login');
-      }
-    });
+    return FutureBuilder<UserPreferences?>(
+      future: ref.read(userDatastoreRepositoryProvider).getUser(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasData && snapshot.data?.token != null) {
+          // User is logged in, continue showing HomeScreen
+          return homeScreenContent(context, ref, snapshot.data!);
+        } else {
+          // No valid session, redirect to SignInScreen
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            ref.read(routerProvider).goNamed('login');
+          });
+          return const SizedBox
+              .shrink(); // Return an empty widget or a loading indicator as needed
+        }
+      },
+    );
+  }
 
-    final homeState = ref.watch(homeStateNotifierProvider);
+  Widget homeScreenContent(
+      BuildContext context, WidgetRef ref, UserPreferences userPreferences) {
+    final userName = userPreferences.name ?? "User";
 
-    if (homeState.isLoading) {
-      return const Center(
-        child: CircularProgressIndicator(),
-      );
-    } else if (homeState.error != null) {
-      return Center(
-        child: Text(homeState.error.toString()),
-      );
-    } else {
-      Widget header() {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Hello, ${homeState.user?.username ?? "User"}",
-                      style: getGrotesqueSemiBoldStyle24(color: neutralFour)),
-                  const SizedBox(
-                    height: 4.0,
-                  ),
-                  Text(
-                    "Let's see what can i do for you",
-                    style: getSansFranciscoRegular14(color: neutralThree),
-                  )
-                ],
-              ),
-              InkWell(
-                  onTap: () {
-                    ref.read(routerProvider).goNamed('setting');
-                  },
-                  child: Image.asset("assets/images/dummy_avatar.png",
-                      width: 50, height: 50)),
-            ]),
-            const SizedBox(
-              height: 24.0,
-            ),
-            Text(
-              "Start a new chat with",
-              style: getGrotesqueSemiBoldStyle40(color: neutralFour),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ShaderMask(
-                  shaderCallback: (Rect bounds) {
-                    return const LinearGradient(
-                      colors: <Color>[
-                        Color(0xFFffcf00), // primary color
-                        Color(0xFFE8A089), // tumbleweed color
-                      ],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                    ).createShader(bounds);
-                  },
-                  blendMode: BlendMode
-                      .srcIn, // This blend mode applies the gradient only to the text
-                  child: Text(
-                    "Elara AI",
-                    style: getGrotesqueSemiBoldStyle40(
-                        color: Colors.white), // Temporarily set to white
-                  ),
-                ),
-                SizedBox(
-                  width: 156,
-                  height: 50,
-                  child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: primary,
-                          shape: const RoundedRectangleBorder(
-                              borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(36),
-                            bottomLeft: Radius.circular(36),
-                            topRight: Radius.circular(36),
-                            bottomRight: Radius.circular(8),
-                          ))),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Icon(
-                            Icons.add,
-                            size: 24,
-                            color: neutralFour,
-                          ),
-                          Text(
-                            "New Topic",
-                            style: getSansFranciscoBold16(color: neutralFour),
-                          ),
-                        ],
-                      )),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 32.0,
-            ),
-            Row(
-              children: [
-                Flexible(
-                  child: Divider(
-                    color: neutralThree30,
-                    height: 1,
-                    thickness: 1,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        );
-      }
-
-      Widget history() {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 24.0,
-            ),
-            Text("History",
-                style: getSansFranciscoSemiBold24(color: neutralFour)),
-            const SizedBox(
-              height: 24.0,
-            ),
-            const Row(
-              children: [
-                ChatRoomCard(),
-                SizedBox(
-                  width: 8.0,
-                ),
-                ChatRoomCard(),
-              ],
-            )
-          ],
-        );
-      }
-
-      return Scaffold(
-          backgroundColor: neutralOneAlt,
-          body: SafeArea(
-              child: SingleChildScrollView(
-            padding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 24.0),
-            child: Column(
+    Widget header() {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [header(), history()],
+              children: [
+                Text("Hello, $userName",
+                    style: getGrotesqueSemiBoldStyle24(color: neutralFour)),
+                const SizedBox(
+                  height: 4.0,
+                ),
+                Text(
+                  "Let's see what can i do for you",
+                  style: getSansFranciscoRegular14(color: neutralThree),
+                )
+              ],
             ),
-          )));
+            InkWell(
+                onTap: () {
+                  ref.read(routerProvider).goNamed('setting');
+                },
+                child: Image.asset("assets/images/dummy_avatar.png",
+                    width: 50, height: 50)),
+          ]),
+          const SizedBox(
+            height: 24.0,
+          ),
+          Text(
+            "Start a new chat with",
+            style: getGrotesqueSemiBoldStyle40(color: neutralFour),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ShaderMask(
+                shaderCallback: (Rect bounds) {
+                  return const LinearGradient(
+                    colors: <Color>[
+                      Color(0xFFffcf00), // primary color
+                      Color(0xFFE8A089), // tumbleweed color
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ).createShader(bounds);
+                },
+                blendMode: BlendMode
+                    .srcIn, // This blend mode applies the gradient only to the text
+                child: Text(
+                  "Elara AI",
+                  style: getGrotesqueSemiBoldStyle40(
+                      color: Colors.white), // Temporarily set to white
+                ),
+              ),
+              SizedBox(
+                width: 156,
+                height: 50,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: primary,
+                        shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(36),
+                          bottomLeft: Radius.circular(36),
+                          topRight: Radius.circular(36),
+                          bottomRight: Radius.circular(8),
+                        ))),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(
+                          Icons.add,
+                          size: 24,
+                          color: neutralFour,
+                        ),
+                        Text(
+                          "New Topic",
+                          style: getSansFranciscoBold16(color: neutralFour),
+                        ),
+                      ],
+                    )),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 32.0,
+          ),
+          Row(
+            children: [
+              Flexible(
+                child: Divider(
+                  color: neutralThree30,
+                  height: 1,
+                  thickness: 1,
+                ),
+              ),
+            ],
+          ),
+        ],
+      );
     }
+
+    Widget history() {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 24.0,
+          ),
+          Text("History",
+              style: getSansFranciscoSemiBold24(color: neutralFour)),
+          const SizedBox(
+            height: 24.0,
+          ),
+          const Row(
+            children: [
+              ChatRoomCard(),
+              SizedBox(
+                width: 8.0,
+              ),
+              ChatRoomCard(),
+            ],
+          )
+        ],
+      );
+    }
+
+    return Scaffold(
+        backgroundColor: neutralOneAlt,
+        body: SafeArea(
+            child: SingleChildScrollView(
+          padding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [header(), history()],
+          ),
+        )));
   }
 }
--- lib/feature_assistant/presentation/home/home_state_notifier.dart
+++ lib/feature_assistant/presentation/home/home_state_notifier.dart
@@ -1,31 +1,32 @@
 import 'package:elarise/core/common/result_state.dart';
-import 'package:elarise/di/usecases/auth_usecases/get_logged_in_user_provider.dart';
 import 'package:elarise/feature_assistant/presentation/home/home_state.dart';
-import 'package:elarise/feature_auth/domain/usecases/get_logged_in_user/get_logged_in_user.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 
 class HomeStateNotifier extends StateNotifier<HomeState> {
   final Ref ref;
 
-  HomeStateNotifier(this.ref) : super(HomeState(isLoading: true)) {
-    _fetchUserData();
+  HomeStateNotifier(this.ref) : super(HomeState(isLoading: false, user: null)) {
+    // _fetchUserData();
   }
 
-  Future<void> _fetchUserData() async {
-    try {
-      GetLoggedInUser useCase = ref.read(getLoggedInUserProvider);
-      var result = await useCase(null);
+  // Future<void> _fetchUserData() async {
+  //   try {
+  //     GetLoggedInUser useCase = ref.read(getLoggedInUserProvider);
+  //     var result = await useCase(null);
 
-      if (result is Success) {
-        state = state.copyWith(user: result.resultData, isLoading: false);
-      } else {
-        state = state.copyWith(user: null, isLoading: false);
-      }
-    } catch (e) {
-      state = state.copyWith(error: e.toString(), isLoading: false);
-    }
-  }
+  //     if (result is Success) {
+  //       state = HomeState(
+  //           user: result.resultData, isLoading: false, error: null);
+  //     } else {
+  //       state = HomeState(
+  //           user: null,
+  //           isLoading: false,
+  //           error: result.errorMessage ?? 'User not found');
+  //     }
+  //   } catch (e) {
+  //     state = HomeState(error: e.toString(), isLoading: false, user: null);
+  //   }
+  // }
 }
 
 final homeStateNotifierProvider =
