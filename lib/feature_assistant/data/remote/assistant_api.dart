9fef9b30b5aaa6b0316dd637b2bf9bb241e11588
Rayhan Illyas <rayhan205732@gmail.com>
2024-03-10 17:37:03 +0700

    feat(assistant): data flow for get all freely talk room

--- lib/feature_assistant/data/remote/assistant_api.dart
+++ lib/feature_assistant/data/remote/assistant_api.dart
@@ -1,5 +1,6 @@
 import 'package:elarise/core/data/remote/api_config.dart';
 import 'package:elarise/feature_assistant/domain/entities/chatroom_voice_response.dart';
+import 'package:elarise/feature_assistant/domain/entities/get_all_talk_freely_response.dart';
 import 'package:elarise/feature_assistant/domain/entities/talk_freely_response.dart';
 
 class AssistantApi {
@@ -45,6 +46,30 @@ class AssistantApi {
       return response;
     } catch (e) {
       throw Exception('Failed to freely talk chat: $e');
+    }
+  }
+
+  Future<List<TalkFreelyChatRoom>> getAllFreelyTalkRooms() async {
+    try {
+      var headers = {
+        'Content-Type': 'application/json',
+      };
+
+      final response = await apiConfig.getApiService<GetAllTalkFreelyResponse>(
+          'get-all-chatroom-voice',
+          headers: headers,
+          decoder: (json) => GetAllTalkFreelyResponse.fromJson(json));
+
+      // Create a new modifiable list from the response data
+      List<TalkFreelyChatRoom> newResponse =
+          List<TalkFreelyChatRoom>.from(response.data);
+
+      // Sort the modifiable list by the createdAt field, newest first
+      newResponse.sort((a, b) => b.createdAt.compareTo(a.createdAt));
+
+      return newResponse;
+    } catch (e) {
+      throw Exception('Failed to get all freely talk chat rooms: $e');
     }
   }
 }
--- lib/feature_assistant/data/repositories_impl/assistant_repository_impl.dart
+++ lib/feature_assistant/data/repositories_impl/assistant_repository_impl.dart
@@ -2,6 +2,7 @@ import 'package:elarise/core/common/result_state.dart';
 import 'package:elarise/feature_assistant/data/remote/openai_service.dart';
 import 'package:elarise/feature_assistant/data/repositories/assistant_repository.dart';
 import 'package:elarise/feature_assistant/domain/entities/chatroom_voice_response.dart';
+import 'package:elarise/feature_assistant/domain/entities/get_all_talk_freely_response.dart';
 import 'package:elarise/feature_assistant/domain/entities/talk_freely_response.dart';
 
 import '../../../core/data/repositories/user_datastore_repository.dart';
@@ -55,6 +56,24 @@ class AssistantRepositoryImpl implements AssistantRepository {
       // Call text-to-speech service with the chat response
       await _openAIService.fetchAndPlaySpeechAudio(message);
 
+      return ResultState.success(response);
+    } catch (e) {
+      return ResultState.error(e.toString());
+    }
+  }
+
+  @override
+  Future<ResultState<List<TalkFreelyChatRoom>>>
+      getAllFreelyTalkRooms() async {
+    try {
+      final userPreferences = await _userDatastoreRepository.getUser();
+      final token = userPreferences.token ?? '';
+
+      if (token.isEmpty) {
+        return const ResultState.error('Token is empty');
+      }
+
+      final response = await _assistantApi.getAllFreelyTalkRooms();
       return ResultState.success(response);
     } catch (e) {
       return ResultState.error(e.toString());
