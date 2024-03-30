83aa60029c3cdcfaf07e01615bb84552c1bf9f79
Rayhan Illyas <rayhan205732@gmail.com>
2024-03-30 15:48:08 +0700

    feat: add global state notifier

---
+++ lib/global/global_state_notifier.dart
@@ -0,0 +1,30 @@
+import 'package:elarise/global/global_state.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../di/repositories/user_datastore_repository/user_datastore_repository_provider.dart';
+
+class GlobalStateNotifier extends StateNotifier<GlobalState> {
+  final Ref ref;
+
+  GlobalStateNotifier(this.ref) : super(GlobalState()) {
+    _loadUserPreferences();
+  }
+
+  Future<void> _loadUserPreferences() async {
+    state = state.copyWith(isLoading: true);
+
+    try {
+      final userPreferences =
+          await ref.read(userDatastoreRepositoryProvider).getUser();
+      state =
+          state.copyWith(userPreferences: userPreferences, isLoading: false);
+    } catch (e) {
+      state = state.copyWith(error: e.toString(), isLoading: false);
+    }
+  }
+}
+
+final globalStateNotifierProvider =
+    StateNotifierProvider<GlobalStateNotifier, GlobalState>((ref) {
+  return GlobalStateNotifier(ref);
+});
